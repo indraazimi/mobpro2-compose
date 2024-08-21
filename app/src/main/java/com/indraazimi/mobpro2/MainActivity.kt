183a023582938bc1db6f6eebf2af67cb44344bd5
Rahman Hakim <rahmanhakim2435@gmail.com>
2024-08-21 13:41:53 +0700

    optimize import

--- app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
+++ app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
