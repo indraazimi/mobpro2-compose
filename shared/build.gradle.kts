38ae8f1f102fa5b53e73386ec64432a83c0debd5
Indra Azimi <indraazimi@yahoo.com>
2024-10-06 22:09:52 +0700

    Menambahkan kode untuk login Google

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -47,11 +47,14 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.ui.auth)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
