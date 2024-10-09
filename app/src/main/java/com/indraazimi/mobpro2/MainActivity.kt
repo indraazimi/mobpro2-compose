8ad6b0296cf498a162145aa560d4cccef916bd44
Indra Azimi <indraazimi@yahoo.com>
2024-10-09 21:17:39 +0700

    Update UI berdasarkan status login

--- app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
+++ app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
@@ -13,12 +13,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.indraazimi.mobpro2.ui.screen.app.DosenApp
 import com.indraazimi.mobpro2.ui.theme.Mobpro2Theme
 import com.indraazimi.mobpro2s.ui.WelcomeScreen
 
@@ -28,13 +25,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobpro2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    WelcomeScreen(
-                        appLogo = R.mipmap.ic_launcher,
-                        appName = R.string.app_name,
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DosenApp()
             }
         }
     }
