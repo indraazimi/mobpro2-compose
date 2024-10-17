00ed3b6f6254fef6353716843ce13ece0a10607d
Indra Azimi <indraazimi@yahoo.com>
2024-10-17 11:35:04 +0700

    Membuat dialog tambah kelas

--- app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainScreen.kt
@@ -11,9 +11,19 @@ package com.indraazimi.mobpro2.ui.screen.main
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.google.firebase.auth.FirebaseUser
 import com.indraazimi.mobpro2.R
@@ -24,8 +34,19 @@ import com.indraazimi.mobpro2s.ui.UserProfileCard
 fun MainScreen(
     user: FirebaseUser
 ) {
+    var showDialog by remember { mutableStateOf(false) }
+
     Scaffold(
-        topBar = { AppBarWithLogout(R.string.app_name) }
+        topBar = { AppBarWithLogout(R.string.app_name) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { showDialog = true }) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.tambah_kelas),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
     ) { innerPadding ->
         Column(
             modifier = Modifier
@@ -33,6 +54,12 @@ fun MainScreen(
                 .padding(16.dp)
         ) {
             UserProfileCard(user)
+        }
+
+        if (showDialog) {
+            KelasDialog(onDismissRequest = { showDialog = false }) {
+                showDialog = false
+            }
         }
     }
 }
