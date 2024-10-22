57fcbb8a4cd17774ce8ca8b5018d95ae585b85dc
Indra Azimi <indraazimi@yahoo.com>
2024-10-22 12:05:01 +0700

    Mengambil data kelas dari database

--- app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainScreen.kt
+++ app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainScreen.kt
@@ -9,14 +9,20 @@
 
 package com.indraazimi.mobpro2.ui.screen.main
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -52,12 +58,31 @@ fun MainScreen(
             }
         }
     ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(16.dp)
+        LazyColumn(
+            modifier = Modifier.padding(innerPadding),
+            contentPadding = PaddingValues(bottom = 84.dp)
         ) {
-            UserProfileCard(user)
+            item {
+                UserProfileCard(
+                    user = user,
+                    modifier = Modifier.padding(16.dp)
+                )
+                HorizontalDivider(
+                    modifier = Modifier.padding(horizontal = 24.dp)
+                )
+            }
+
+            items(viewModel.data) {
+                Card(
+                    modifier = Modifier.fillMaxWidth()
+                        .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+                ) {
+                    Text(
+                        text = it.nama,
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp)
+                    )
+                }
+            }
         }
 
         if (showDialog) {
--- app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainViewModel.kt
+++ app/src/main/java/com/indraazimi/mobpro2/ui/screen/main/MainViewModel.kt
@@ -9,16 +9,72 @@
 
 package com.indraazimi.mobpro2.ui.screen.main
 
+import android.util.Log
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.ListenerRegistration
+import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
 import com.indraazimi.mobpro2s.model.Kelas
 
 class MainViewModel(private val uid: String) : ViewModel() {
 
     private val db = Firebase.firestore
 
+    var data = mutableStateListOf<Kelas>()
+        private set
+
+    private var registration: ListenerRegistration? = null
+
+    private val listener = object : EventListener<QuerySnapshot> {
+        override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+            if (error != null) {
+                Log.e("MainViewModel", "onEvent:error", error)
+                return
+            }
+
+            value?.documentChanges?.forEach { handle(it) }
+        }
+    }
+
+    private fun handle(change: DocumentChange) {
+        when (change.type) {
+            DocumentChange.Type.ADDED -> {
+                val kelas = change.document.toObject<Kelas>()
+                data.add(change.newIndex, kelas)
+            }
+            DocumentChange.Type.MODIFIED -> {
+                val kelas = change.document.toObject<Kelas>()
+                if (change.oldIndex == change.newIndex) {
+                    data[change.oldIndex] = kelas
+                } else {
+                    data.removeAt(change.oldIndex)
+                    data.add(change.newIndex, kelas)
+                }
+            }
+            DocumentChange.Type.REMOVED -> {
+                data.removeAt(change.oldIndex)
+            }
+        }
+    }
+
+    init {
+        registration = db.collection(Kelas.COLLECTION)
+            .addSnapshotListener(listener)
+    }
+
     fun insert(nama: String) {
         db.collection(Kelas.COLLECTION).add(Kelas(uid, nama))
+    }
+
+    override fun onCleared() {
+        registration?.remove()
+        registration = null
+        super.onCleared()
     }
 }
