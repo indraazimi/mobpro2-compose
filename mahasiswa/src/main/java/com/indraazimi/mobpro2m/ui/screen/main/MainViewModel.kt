300bafabb3c27480a8808993cf92f8ff8ff29e2f
Indra Azimi <indraazimi@yahoo.com>
2024-11-01 21:45:40 +0700

    Membuat dropdown kelas mahasiswa

---
+++ mahasiswa/src/main/java/com/indraazimi/mobpro2m/ui/screen/main/MainViewModel.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk buku berjudul "Pemrograman Android Lanjut".
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2m.ui.screen.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    val dataKelas = listOf(
+        "D3IF-46-02",
+        "D3IF-46-03",
+    )
+}
