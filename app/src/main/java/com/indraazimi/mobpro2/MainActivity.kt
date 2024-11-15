710d11d0bc5545b5cb17c7ed2f554f7f20f2cd73
Indra Azimi <indraazimi@yahoo.com>
2024-11-15 21:26:27 +0700

    Menambahkan TopAppBar

--- app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
+++ app/src/main/java/com/indraazimi/mobpro2/MainActivity.kt
@@ -13,13 +13,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
+import com.indraazimi.mobpro2.ui.screen.MainScreen
 import com.indraazimi.mobpro2.ui.theme.Mobpro2Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,29 +32,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Mobpro2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Mobpro2App()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobpro2Theme {
-        Greeting("Android")
+fun Mobpro2App() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(R.string.app_name)) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        }
+    ) { innerPadding ->
+        MainScreen(Modifier.padding(innerPadding))
     }
 }
---
+++ app/src/main/java/com/indraazimi/mobpro2/ui/screen/MainScreen.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk buku berjudul "Pemrograman Android Lanjut".
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.mobpro2.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(
+    modifier: Modifier
+) {
+
+}
