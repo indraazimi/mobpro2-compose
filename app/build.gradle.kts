bcad6ac64a02f2d9b170655a521de98d362eaae0
Indra Azimi <indraazimi@yahoo.com>
2024-11-15 21:28:10 +0700

    Membuat tampilan peta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.ovh.plrapps.mapcompose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
