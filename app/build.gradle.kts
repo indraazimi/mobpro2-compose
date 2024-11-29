9e79706ce3877e97a4fbc965d5fac639997649c5
Indra Azimi <indraazimi@yahoo.com>
2024-11-29 16:31:22 +0700

    Meminta izin lokasi pengguna

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.ovh.plrapps.mapcompose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.accompanist.permissions)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/indraazimi/mobpro2/ui/screen/MainScreen.kt
+++ app/src/main/java/com/indraazimi/mobpro2/ui/screen/MainScreen.kt
@@ -9,14 +9,19 @@
 
 package com.indraazimi.mobpro2.ui.screen
 
+import android.Manifest
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import com.indraazimi.mobpro2.model.Coordinate
 import ovh.plrapps.mapcompose.api.scrollTo
 import ovh.plrapps.mapcompose.ui.MapUI
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun MainScreen(
     modifier: Modifier
@@ -24,9 +29,17 @@ fun MainScreen(
     val viewModel: MainViewModel = viewModel()
     val mapState = viewModel.mapState
 
+    val locationPermissionState = rememberPermissionState(
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
     LaunchedEffect(Unit) {
         val pos = Coordinate(-6.973377, 107.631543)
         mapState.scrollTo(pos.xScaled, pos.yScaled)
+
+        if (!locationPermissionState.status.isGranted) {
+            locationPermissionState.launchPermissionRequest()
+        }
     }
 
     MapUI(
